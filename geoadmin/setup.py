86a08cbad64c8817675d31ebf7988d81871d94c2
c2cfredj <c2cfredj@a186c27d-65e8-40a5-ab69-11a960a16feb>
2010-09-30 09:32:23 +0000

    i18n basic setup
    git-svn-id: https://svn.bgdi.admin.ch/mf-geoadmin2/trunk@12 a186c27d-65e8-40a5-ab69-11a960a16feb

--- geoadmin/setup.py
+++ geoadmin/setup.py
@@ -13,21 +13,18 @@ setup(
     author_email='',
     url='',
     install_requires=[
-        "psycopg2>=2.2.0,<=2.2.99",
         "mapfish>=2.0,<=2.0.99",
-        "httplib2>=0.6.0,<=0.6.99",
-        "Babel<=0.9.99",
-        "TileCache>=2.10,<=2.10.99"
+        "Babel<=0.9.99"
     ],
     #setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     test_suite='nose.collector',
     package_data={'geoadmin': ['i18n/*/LC_MESSAGES/*.mo']},
-    #message_extractors={'geoadmin': [
-    #        ('**.py', 'python', None),
-    #        ('templates/**', 'mako', {'input_encoding': 'utf-8'}),
-    #        ('public/**', 'ignore', None)]},
+    message_extractors={'geoadmin': [
+            ('**.py', 'python', None),
+            ('templates/**', 'mako', {'input_encoding': 'utf-8'}),
+            ('public/**', 'ignore', None)]},
     zip_safe=False,
     #paster_plugins=['MapFish', 'PasteScript', 'Pylons', 'geoadmin'],
     entry_points="""
